849e165a9af3f7b0db343fa38050b5e59d10085f
Robert Bunch <rdbunch@gmail.com>
2023-11-17 13:50:51 -0500

    add cors policy to starter

---
+++ starterFiles/s3CorsPolicy.js
@@ -0,0 +1,17 @@
+[
+    {
+        "AllowedHeaders": [
+            "*"
+        ],
+        "AllowedMethods": [
+            "GET",
+            "HEAD",
+            "PUT" //This is REQUIRED for this exercise
+        ],
+        "AllowedOrigins": [
+            "http://localhost:3000" //Change this to your domain
+        ],
+        "ExposeHeaders": [],
+        "MaxAgeSeconds": 3000
+    }
+]
